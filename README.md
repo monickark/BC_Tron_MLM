468208a9bc035cf89bd669b5a6f55d50da910f14
monickark <monicka.rk@gmail.com>
2024-10-06 15:47:27 +0530

    Added user registration

--- README.md
+++ README.md
@@ -2,9 +2,29 @@
 
 ## Contract Deployment
 
-Function : Contract deployment	\
-Msg.sender : TJHYbk7q2EuMJJZeEF6cxPBEDg9kG1sR1j \
-msg.value : 0 \
-_referrerID : 0
+### Function call Values
+    Function : Contract deployment	\
+    Msg.sender : TJHYbk7q2EuMJJZeEF6cxPBEDg9kG1sR1j \
+    msg.value : 0 \
+    _referrerID : 0
 
-![Function @ Deployment](<1.ContractDeployment.png>)
+![Function @ Deployment](<images/1.ContractDeployment.png>)
+
+## User Registration
+### Function call Values
+    Function : regUser	\
+    Msg.sender : TRc7JCUtMopM3sADYDj5KUBhzD1K3q1JsR \
+    msg.value : 150 \
+    _referrerID : 1
+
+    *Tron Balance Before*
+    TJHYbk7q2EuMJJZeEF6cxPBEDg9kG1sR1j : 1000000
+    TRc7JCUtMopM3sADYDj5KUBhzD1K3q1JsR : 1000000
+
+    *Tron Balance After*
+    TJHYbk7q2EuMJJZeEF6cxPBEDg9kG1sR1j : 1000150
+    TRc7JCUtMopM3sADYDj5KUBhzD1K3q1JsR : 999850
+![User Registration](<images/2.UserRegistration.png>)
+
+### After added 3 people in level1 of user1 who has owner as referrer
+![Level1TrxBalAfter3referrer](<images/3.Level1TrxBalAfter3referrer.png>)
--- SmartGenie2.0.sol
+++ SmartGenie2.0.sol
@@ -16,15 +16,19 @@ contract SmartGenie {
     mapping(uint => uint) public LEVEL_PRICE;
     mapping (address => UserStruct) public users;
     mapping (uint => address) public userList;
-
-
     
     uint public currUserID = 0;
-
     
-     constructor() public {
+    event regLevelEvent(address indexed _user, address indexed _referrer, uint _time);
+    event getMoneyForLevelEvent(address indexed _user, address indexed _referral, uint _level, uint _time);
+    event lostMoneyForLevelEvent(address indexed _user, address indexed _referral, uint _level, uint _time);
+    
+    constructor() public {
+         
+        // Contract deployer will be the owner wallet 
         ownerWallet = msg.sender;
 
+        // Setting the price for buying each level
         LEVEL_PRICE[1] = 150 trx;
         LEVEL_PRICE[2] = LEVEL_PRICE[1]*2 trx;
         LEVEL_PRICE[3] = LEVEL_PRICE[2]*2 trx;
@@ -36,6 +40,7 @@ contract SmartGenie {
         LEVEL_PRICE[9] = LEVEL_PRICE[8]*2 trx;
         LEVEL_PRICE[10] = LEVEL_PRICE[9]*2 trx;
 
+        // Create contract deployer as first user
         UserStruct memory userStruct;
         currUserID++;
 
@@ -49,9 +54,68 @@ contract SmartGenie {
         users[ownerWallet] = userStruct;
         userList[currUserID] = ownerWallet;
 
-        for(uint i = 1; i <= 10; i++) {
-            users[ownerWallet].levelExpired[i] = 55555555555;
-        }
     }
+    
+    
+    // User Registraion must provide Refferrer Id
+    function regUser(uint _referrerID) public payable {
+        // Caller should not registered already, so his existence in 'users'
+        require(!users[msg.sender].isExist, 'User exist');
+        // Referrer is should not be empty or caller's own id
+        require(_referrerID > 0 && _referrerID <= currUserID, 'Incorrect referrer Id');
+        // Caller must provide first level 'LEVEL_PRICE' for registration
+        require(msg.value == LEVEL_PRICE[1], 'Incorrect Value');
+
+        
+        // Conditions verified. Now Registering user
+        UserStruct memory userStruct;
+        currUserID++;
+        
+        // Cretaing user object
+        userStruct = UserStruct({
+            isExist: true,
+            id: currUserID,
+            referrerID: _referrerID,
+            referral: new address[](0),
+            joined:now
+        });
+        users[msg.sender] = userStruct;
+        
+        // Add new user to existing userlist
+        userList[currUserID] = msg.sender;
+
+        // Add the new user as referral for the given referrer id
+        users[userList[_referrerID]].referral.push(msg.sender);
+        
+        // Payment for the level
+        payForLevel(1, msg.sender);
+        
+        // registration done. Emit event
+        emit regLevelEvent(msg.sender, userList[_referrerID], now);
+    }
+    
+    
+    // Payment function for a level
+    function payForLevel(uint _level, address _user) internal {
+        address referer;
+        
+        // Check level and get referrer id for the user
+        if(_level == 1) {
+            referer = userList[users[_user].referrerID];
+        }
+        
+        bool sent = false;
+            sent = address(uint160(referer)).send(LEVEL_PRICE[_level]);
+
+            if (sent) {
+                emit getMoneyForLevelEvent(referer, msg.sender, _level, now);
+            }
+            if(!sent) {
+                emit lostMoneyForLevelEvent(referer, msg.sender, _level, now);
+    
+                payForLevel(_level, referer);
+            }
+    }
+    
     
 }
